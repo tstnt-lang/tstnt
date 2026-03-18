5c97bbd179091cf5a123457b39abb7fbd7a9bce4
tstnt-dev <t3stn3tmeow@gmail.com>
2026-03-18 21:57:04 +0100

    v1.0.5 — fix module.func() calls

--- target/release/deps/tstnt_lsp-6fdb98e0f59d0885.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/tstnt/target/release/deps/tstnt_lsp-6fdb98e0f59d0885.d: src/lsp/main.rs
-
-/data/data/com.termux/files/home/tstnt/target/release/deps/tstnt_lsp-6fdb98e0f59d0885: src/lsp/main.rs
-
-src/lsp/main.rs:
--- target/release/tstnt-lsp.d
+++
@@ -1,1 +0,0 @@
-/data/data/com.termux/files/home/tstnt/target/release/tstnt-lsp: /data/data/com.termux/files/home/tstnt/src/lsp/main.rs
